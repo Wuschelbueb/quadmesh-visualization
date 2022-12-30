e2d59c2483d2a034e6499c570a2b26e0e399cc51
wuschelbueb <Wuschelbueb@users.noreply.github.com>
2022-12-30 17:56:16 +0100

    directional lighting + phong shading

--- Glitter/Headers/glitter.hpp
+++ Glitter/Headers/glitter.hpp
@@ -45,5 +45,7 @@ void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+unsigned int loadTexture(const char *path);
+
 
 #endif //~ Glitter Header
--- Glitter/Shaders/shader.fs
+++ Glitter/Shaders/shader.fs
@@ -1,7 +1,29 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 FragPos;  
+in vec3 Normal;  
 in vec2 TexCoord;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;    
+    float shininess;
+}; 
+
+struct Light {
+    //vec3 position;
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform vec3 viewPos;
+uniform vec3 objectColor;
+uniform Material material;
+uniform Light light;
 
 // texture samplers
 uniform sampler2D texture1;
@@ -15,7 +37,7 @@ uniform float center_v;
 uniform float scale_u;
 uniform float scale_v;
 
-// // Number of elements along each axis
+// Number of elements along each axis
 uniform int elements_u;
 uniform int elements_v;
 
@@ -34,12 +56,34 @@ void main()
 	offset = (offset - TexCoord) * mat2(cos_factor, sin_factor, -sin_factor, cos_factor);
 	offset += TexCoord;
 
+    // ambient
+    vec3 ambient = light.ambient * light.diffuse; // * texture(material.diffuse, TexCoord).rgb; 
+      	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    // vec3 lightDir = normalize(light.position - FragPos);
+    vec3 lightDir = normalize(-light.direction);  
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff; // * texture(material.diffuse, TexCoord).rgb;
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec; // * texture(material.specular, TexCoord).rgb;
+        
+    // vec3 result = (ambient + diffuse + specular)*objectColor;
+    // FragColor = vec4(result, 1.0);
+
     // Do texture lookup based on the vertices texture coordinates
     vec4 bg_color = texture(texture1, TexCoord);
+    vec3 tempLighting = (ambient + diffuse + specular);
     vec4 quad_texture = texture(texture2, offset);
+    vec4 bgColorEnd = vec4(tempLighting * objectColor, 1.0) * bg_color;
+    vec4 quadTexEnd = vec4(tempLighting, 1.0) * quad_texture;
 	if (all(lessThan(abs(offset), bounds))) {
-	    FragColor = mix(bg_color,quad_texture, 0.9);
+	    FragColor = mix(bgColorEnd,quadTexEnd, 0.9);
 	} else {
-	    FragColor = bg_color;
+	    FragColor = bgColorEnd;
 	}
 }
